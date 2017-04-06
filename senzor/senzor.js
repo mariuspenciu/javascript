cdac93515d3d495436bd9535f84c1538c9282354
mariuspenciu <penciumarius@gmail.com>
2017-04-06 19:47:44 +0300

    desenare cerc

--- senzor/senzor.js
+++ senzor/senzor.js
@@ -10,4 +10,16 @@ function on_device_orientation(evt)
 	document.getElementById("a").innerHTML = "alpha = " + alpha.toString();
 	document.getElementById("b").innerHTML = "beta = " + beta.toString();
 	document.getElementById("g").innerHTML = "gamma = " + gamma.toString();
+	
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	var raza = 20;
+	var centru = {x:canvas.width / 2, y:canvas.height / 2};
+	
+	ctx.beginPath();
+	ctx.arc(centru.x + gamma * (canvas.width / 2 - raza) / 90, centru.y + beta * (canvas.height / 2 - raza) / 90, raza, 0, 2 * Math.PI);
+	ctx.stroke();
 }
