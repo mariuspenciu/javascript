e72f94ab386469e329531776976c29341325e4b4
mariuspenciu <penciumarius@gmail.com>
2017-05-04 20:08:56 +0300

    cercuri colorate

--- senzor/svg2.js
+++ senzor/svg2.js
@@ -24,8 +24,8 @@ function on_touch_start(evt)
 		
 		var color = random_color();
 		
-		c.setAttribute("stroke", "red");
-		c.setAttribute("fill", "red");
+		c.setAttribute("stroke", color);
+		c.setAttribute("fill", color);
 		svg.appendChild(c);
 		
 		
