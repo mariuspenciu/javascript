a44493d403739dd1aff25e26cb701d8c18d67c54
mariuspenciu <penciumarius@gmail.com>
2017-05-04 19:14:40 +0300

    doua degete doua cercuri

--- senzor/svg.js
+++ senzor/svg.js
@@ -1,13 +1,22 @@
 var c1 = document.getElementById("cerc1");
+var c2 = document.getElementById("cerc2");
 
 c1.addEventListener("touchmove", on_touch_move);
+c2.addEventListener("touchmove", on_touch_move);
 
 function on_touch_move(e)
 
-{
+
+{	
+	for (var i = 0; i < e.changedTouches.length; i++){
+		
 	var t = e.changedTouches[0];
-	c1.setAttribute("cx", t.pageX);
-	c1.setAttribute("cy", t.pageY);
+	var c = t.target;
+	c.setAttribute("cx", t.pageX);
+	c.setAttribute("cy", t.pageY);
+	}	
+	
 	
 	
 }
+
