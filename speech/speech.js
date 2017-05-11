f55eba8087a67a7224d353f8fe2d861f4009fc23
mariuspenciu <penciumarius@gmail.com>
2017-05-11 18:17:06 +0300

    alternatives

--- speech/speech.js
+++ speech/speech.js
@@ -6,22 +6,34 @@ document.addEventListener("mousedown", on_touch);
  
  var recognition = new webkitSpeechRecognition();
  recognition.lang = 'en-US';
-
+ recognition.maxAlternative = 5;
+ 
+ var recognition_started = false;
  
  function on_touch(e)
- {
+ {	
+	if (!recognition_started){
 	 recognition.start();
+	 recognition_started = true;
+	 
+	}
+	
  }
  function on_end()
  {
 	 recognition.stop();
+	 recognition_started = false;
  }
  
  recognition.onend = on_end;
  recognition.onsoundend = on_end;
  recognition.onspeechend = on_end;
  
+ recognition.onresult = on_results;
+ 
  recognition.onresult = function (e){
-	 document.getElementById("text").innerHTML = e.results[0][0].transcript + " " + e.results[0][0].confidence;
+	 var alternatives = e.results[0];
+	 for (var i = 0; i < alternatives.length; i++)
+	 document.getElementById("text").innerHTML += alternatives[i].transcript + " " + alternatives[i].confidence + "<br>";
  }
  
